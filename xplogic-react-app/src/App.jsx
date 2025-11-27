bd6c2bc0e0590bb0ebd3acde8544e8defcf34624
pdeaville <pydeaville@gmail.com>
2025-11-27 03:05:43 +0000

    adding extremely basic website

---
+++ xplogic-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <nav className="navbar">
+        <a href="/">Home</a>
+      </nav>
+      <h1>XPLogic</h1>
+    </>
+  )
+}
+
+export default App
